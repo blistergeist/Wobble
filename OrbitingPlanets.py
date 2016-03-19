cb90769de27250844114bf42bfb0146b4f2fedd3
blistergeist <morgan.j.allison@gmail.com>
2016-03-18 22:43:34 -0400

    Matplotlib rewrite
    Rewrote OrbitingPlanets.py using matplotlib.pyplot for the graphical section. Greatly increased calculation and plotting speed.

--- OrbitingPlanets.py
+++ OrbitingPlanets.py
@@ -1,59 +1,43 @@
 #!/usr/bin/env python3
 
-import math
-from turtle import *
+import numpy as np
+import matplotlib.pyplot as plt
 
 # The gravitational constant G
 G = 6.67428e-11
 
-# Assumed scale: 100 pixels = 1AU.
+#No need to worry about scale
 AU = (149.6e6 * 1000)     # 149.6 million km, in meters.
-SCALE = 50 / AU
 
-class Body(Turtle):
-    """Subclass of Turtle representing a gravitationally-acting body.
+class heavenly_body:
+    #pos and vel should both be tuples, e.g. (0, 3)
+    #in case you want to add other initial conditions
+    def __init__(self, name='yar', mass=0, pos=(0,0), vel=(0,0), color='k'):
+        self.name = name
+        self.mass = mass
+        self.px, self.py = pos
+        self.vx, self.vy = vel
+        self.pxvector = [self.px]
+        self.pyvector = [self.py]
+        self.color = color
 
-    Extra attributes:
-    mass : mass in kg
-    vx, vy: x, y velocities in m/s
-    px, py: x, y positions in m
-    """
-    
-    name = 'Body'
-    mass = None
-    vx = vy = 0.0
-    px = py = 0.0
-    
     def attraction(self, other):
-        """(Body): (fx, fy)
-
-        Returns the force exerted upon this body by the other body.
-        """
         # Report an error if the other object is the same as this one.
         if self is other:
-            raise ValueError("Attraction of object %r to itself requested"
-                             % self.name)
-
-        # Compute the distance of the other body.
-        sx, sy = self.px, self.py
-        ox, oy = other.px, other.py
-        dx = (ox-sx)
-        dy = (oy-sy)
-        d = math.sqrt(dx**2 + dy**2)
-
-        # Report an error if the distance is zero; otherwise we'll
-        # get a ZeroDivisionError exception further down.
+            raise ValueError("Attraction of object %r to itself requested" % self.name)
+        dx = other.px-self.px
+        dy = other.py-self.py
+        d = np.sqrt(dx**2 + dy**2)
         if d == 0:
-            raise ValueError("Collision between objects %r and %r"
-                             % (self.name, other.name))
-
+            raise ValueError("Collision between objects %r and %r" % (self.name, other.name))
+        
         # Compute the force of attraction
-        f = G * self.mass * other.mass / (d**2)
+        f = G*self.mass*other.mass/(d**2)
 
         # Compute the direction of the force.
-        theta = math.atan2(dy, dx)
-        fx = math.cos(theta) * f
-        fy = math.sin(theta) * f
+        theta = np.arctan2(dy, dx)
+        fx = np.cos(theta) * f
+        fy = np.sin(theta) * f
         return fx, fy
 
 def update_info(step, bodies):
@@ -63,30 +47,43 @@ def update_info(step, bodies):
     """
     print('Step #{0}'.format(step))
     for body in bodies:
-        s = '{0:<8}  Pos.={1:>6.2f} {2:>6.2f} Vel.={3:>10.3f} {4:>10.3f}'.format(
-            body.name, body.px/AU, body.py/AU, body.vx, body.vy)
-        print(s)
-    print()
+         print('{0:<8}  Pos.={1:>6.2f} {2:>6.2f} Vel.={3:>10.3f} {4:>10.3f}'.format(
+            body.name, body.px/AU, body.py/AU, body.vx, body.vy))
+    print('\n')
 
-def loop(bodies):
+def draw_orbits():
+    plt.plot(sun.pxvector, sun.pyvector, 'black')
+    plt.plot(mercury.pxvector, mercury.pyvector, 'orange')
+    plt.plot(earth.pxvector, earth.pyvector, 'blue')
+    plt.plot(venus.pxvector, venus.pyvector, 'yellow')
+    plt.plot(mars.pxvector, mars.pyvector, 'red')
+    plt.plot(jupiter.pxvector, jupiter.pyvector, 'magenta')
+    plt.plot(saturn.pxvector, saturn.pyvector, 'green')
+    plt.plot(uranus.pxvector, uranus.pyvector, 'brown')#haha
+    plt.plot(neptune.pxvector, neptune.pyvector, 'aquamarine')
+    plt.plot(pluto.pxvector, pluto.pyvector, 'purple')
+    plt.show()
+
+def calculate_positions(bodies):
     """([Body])
 
     Never returns; loops through the simulation, updating the
     positions of all the provided bodies.
     """
-    timestep = 24*3600  # One day
-    
-    for body in bodies:
-        body.penup()
-        body.hideturtle()
+    timestep = 24*3600  # One day in seconds
+    num_steps = int(1e5)
+    print('Calculating orbital paths over a period of {} tellurian days...'.format(num_steps))
 
-    step = 1
-    while True:
-        update_info(step, bodies)
-        step += 1
-
+    for step in xrange(num_steps): #columns
+        #update graph every 1000 days
+        if (step>0) and (step%1000 == 0):
+            for body in bodies:
+                plt.plot(body.pxvector,body.pyvector, body.color)
+                body.pxvector = [body.px]
+                body.pyvector = [body.py]
+        #update_info(step, bodies)
         force = {}
-        for body in bodies:
+        for body in bodies:   #rows
             # Add up all of the forces exerted on 'body'.
             total_fx = total_fy = 0.0
             for other in bodies:
@@ -97,94 +94,41 @@ def loop(bodies):
                 total_fx += fx
                 total_fy += fy
 
-            # Record the total force exerted.
-            force[body] = (total_fx, total_fy)
-
-        # Update velocities based upon on the force.
-        for body in bodies:
-            fx, fy = force[body]
-            body.vx += fx / body.mass * timestep
-            body.vy += fy / body.mass * timestep
-
-            # Update positions
-            body.px += body.vx * timestep
-            body.py += body.vy * timestep
-            body.goto(body.px*SCALE, body.py*SCALE)
-            body.dot(3)
-
+            body.vx += total_fx/body.mass*timestep
+            body.vy += total_fy/body.mass*timestep
+            body.px += body.vx*timestep
+            body.py += body.vy*timestep
+            body.pxvector.append(body.px)
+            body.pyvector.append(body.py)
+    plt.show()
 
 def main():
-    sun = Body()
-    sun.name = 'Sun'
-    sun.mass = 1.98892 * 10**30
-    sun.pencolor('black')
+    sun = heavenly_body(name='Sun', mass=1.98892e30)
+    mercury = heavenly_body(name='Mercury', mass=0.3301e24, pos=(-.387*AU,0), vel=(0,47.36e3), color='black')
+    venus = heavenly_body(name='Venus', mass=4.8685e24, pos=(-.723*AU,0), vel=(0,35.02e3), color='orange')
+    earth = heavenly_body(name='Earth', mass=5.9742e24, pos=(-1*AU,0), vel=(0,29.783e3), color='blue')
+    mars = heavenly_body(name='Mars', mass=.64171e24, pos=(-1.524*AU,0), vel=(0,24.07e3), color='red')
+    jupiter = heavenly_body(name='Jupiter', mass=1898.19e24, pos=(-5.204*AU,0), vel=(0,13.06e3), color='magenta')
+    saturn = heavenly_body(name='Saturn', mass=568.34e24, pos=(-9.582*AU,0), vel=(0,9.6e3), color='green')
+    uranus = heavenly_body(name='Uranus', mass=86.813e24, pos=(-19.201*AU,0), vel=(0,6.8e3), color='brown') #haha
+    neptune = heavenly_body(name='Neptune', mass=102.413e24, pos=(-30.047*AU,0), vel=(0,5.43e3), color='aquamarine')
+    pluto = heavenly_body(name='Pluto', mass=0.01303e24, pos=(-39.482*AU,0), vel=(0,4.74e3), color='purple')
 
-    mercury = Body()
-    mercury.name = 'Mercury'
-    mercury.mass = 0.33011 * 10**24 #Planet Mass
-    mercury.px = -0.387 * AU        #Ratio Semimajor axis
-    mercury.vy = 47.36 * 1000       #Planet Mean orbital velocity (km/s)
-    mercury.pencolor('purple')
-
-    # Venus parameters taken from
-    # http://nssdc.gsfc.nasa.gov/planetary/factsheet/venusfact.html
-    venus = Body()
-    venus.name = 'Venus'
-    venus.mass = 4.8685 * 10**24
-    venus.px = -0.723 * AU
-    venus.vy = 35.02 * 1000
-    venus.pencolor('grey')
-
-    earth = Body()
-    earth.name = 'Earth'
-    earth.mass = 5.9742 * 10**24
-    earth.px = -1 * AU
-    earth.vy = 29.783 * 1000            # 29.783 km/sec
-    earth.pencolor('blue')
-
-    mars = Body()
-    mars.name = 'Mars'
-    mars.mass = 0.64171 * 10**24 #Planet Mass
-    mars.px = -1.524 * AU        #Ratio Semimajor axis
-    mars.vy = 24.07 * 1000       #Planet Mean orbital velocity (km/s)
-    mars.pencolor('red')
-
-    jupiter = Body()
-    jupiter.name = 'Jupiter'
-    jupiter.mass = 1898.19 * 10**24 #Planet Mass
-    jupiter.px = -5.204 * AU        #Ratio Semimajor axis
-    jupiter.vy = 13.06 * 1000       #Planet Mean orbital velocity (km/s)
-    jupiter.pencolor('brown')
-
-    saturn = Body()
-    saturn.name = 'Saturn'
-    saturn.mass = 568.34 * 10**24 #Planet Mass
-    saturn.px = -9.582 * AU        #Ratio Semimajor axis
-    saturn.vy = 9.68 * 1000       #Planet Mean orbital velocity (km/s)
-    saturn.pencolor('tan')
-
-    uranus = Body()
-    uranus.name = 'Uranus'
-    uranus.mass = 86.813 * 10**24 #Planet Mass
-    uranus.px = -19.201 * AU        #Ratio Semimajor axis
-    uranus.vy = 6.80 * 1000       #Planet Mean orbital velocity (km/s)
-    uranus.pencolor('blue')
-
-    neptune = Body()
-    neptune.name = 'Neptune'
-    neptune.mass = 102.413 * 10**24 #Planet Mass
-    neptune.px = -30.047 * AU        #Ratio Semimajor axis
-    neptune.vy = 5.43 * 1000       #Planet Mean orbital velocity (km/s)
-    neptune.pencolor('blue')
-
-    pluto = Body()
-    pluto.name = 'Pluto'
-    pluto.mass = 0.01303 * 10**24 #Planet Mass
-    pluto.px = -39.482 * AU        #Ratio Semimajor axis
-    pluto.vy = 4.67 * 1000       #Planet Mean orbital velocity (km/s)
-    pluto.pencolor('orange')
-
-    loop([sun, mercury, venus, earth, mars, jupiter, saturn, uranus, neptune, pluto])
+    bodies = [sun, mercury, venus, earth, mars, jupiter, saturn, uranus, neptune, pluto]
+    calculate_positions(bodies)
+    """
+    plt.plot(sun.pxvector, sun.pyvector, 'black')
+    plt.plot(mercury.pxvector, mercury.pyvector, 'orange')
+    plt.plot(earth.pxvector, earth.pyvector, 'blue')
+    plt.plot(venus.pxvector, venus.pyvector, 'yellow')
+    plt.plot(mars.pxvector, mars.pyvector, 'red')
+    plt.plot(jupiter.pxvector, jupiter.pyvector, 'magenta')
+    plt.plot(saturn.pxvector, saturn.pyvector, 'green')
+    plt.plot(uranus.pxvector, uranus.pyvector, 'brown')#haha
+    plt.plot(neptune.pxvector, neptune.pyvector, 'aquamarine')
+    plt.plot(pluto.pxvector, pluto.pyvector, 'purple')
+    plt.show()
+    """
 
 if __name__ == '__main__':
     main()
